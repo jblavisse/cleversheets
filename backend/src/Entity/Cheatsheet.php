b8379b0c1fbd6f96a4e66c12f3ea8a6aec0539d3
kevin-ledez <kevin.ledez@gmail.com>
2024-10-07 10:32:16 +0200

    chore(symfony) update Cheatsheet entity, add constraints Title

--- backend/src/Entity/Cheatsheet.php
+++ backend/src/Entity/Cheatsheet.php
@@ -13,6 +13,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CheatsheetRepository::class)]
 #[ApiResource]
@@ -52,6 +53,13 @@ class Cheatsheet
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Length(
+        min: 3,
+        max: 100,
+        minMessage: 'This field must have at least {{ limit }} characters.',
+        maxMessage: 'This field cannot have more than {{ limit }} characters.',
+    )]
     #[Groups(['cheatsheet:get_collection', 'cheatsheet:get', 'cheatsheet:create', 'cheatsheet:update'])]
     private ?string $title = null;
 
