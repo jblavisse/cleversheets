1b7b1de692d13e955e017e3bbbe408cf83ad8d63
kevin-ledez <kevin.ledez@gmail.com>
2024-10-21 09:42:22 +0200

    chore(symfony): Edit User Entity, add Annotation

--- backend/src/Entity/User.php
+++ backend/src/Entity/User.php
@@ -2,29 +2,43 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use App\State\GetMeStateProvider as StatesGetMeStateProvider;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
 #[ApiResource]
+#[Get(
+    normalizationContext: ["groups" => ["user:get_me"]],
+    denormalizationContext: ["groups" => ["user:get_me"]],
+    name: "get_me",
+    uriTemplate: "/user-me",
+    provider: StatesGetMeStateProvider::class,
+    security: "is_granted('ROLE_USER')"
+)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(["user:get_me"])]
     private ?int $id = null;
 
     #[ORM\Column(length: 180)]
+    #[Groups(["user:get_me"])]
     private ?string $email = null;
 
     /**
      * @var list<string> The user roles
      */
     #[ORM\Column]
+    #[Groups(["user:get_me"])]
     private array $roles = [];
 
     /**
