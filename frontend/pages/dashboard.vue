36a2e50b85f37740b36e5ecffc4e33f0e6a993c7
kevin-ledez <kevin.ledez@gmail.com>
2024-11-06 14:58:03 +0100

    chore(nuxt): Edit Dashboard page

--- frontend/pages/dashboard.vue
+++ frontend/pages/dashboard.vue
@@ -1,9 +1,24 @@
 <template>
-    <h1>Dashboard</h1>
+    <div>
+        <h1>Dashboard</h1>
+        <Button label="Add Category" class="mr-2" @click="addCategory" />
+        <Button label="Add Cheatsheet" class="mr-2" @click="addCheatsheet" />
+    </div>
 </template>
 
 <script setup>
+import Button from 'primevue/button';
+
 definePageMeta({
     middleware: 'auth'
 });
+
+const router = useRouter();
+
+const addCategory = () => {
+    router.push('/categories/create');
+};
+const addCheatsheet = () => {
+    router.push('/cheatsheets/create');
+};
 </script>
