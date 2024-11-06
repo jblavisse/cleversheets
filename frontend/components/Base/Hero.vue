010f5c7e3dfcfd789e20f4f9518cdfab6fc6aab2
kevin-ledez <kevin.ledez@gmail.com>
2024-11-06 14:57:17 +0100

    chore(nuxt): create Hero homepage

---
+++ frontend/components/Base/Hero.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="bg-surface-0 dark:bg-surface-900">
+        <div class="items-center justify-center">
+            <div class="p-6 pt-12 lg:p-12">
+                <h1 class="text-3xl lg:text-5xl font-bold text-surface-900 dark:text-surface-0 mb-4 lg:leading-normal text-center lg:text-center">
+                    CheatSheet
+                </h1>
+                <p class="text-surface-700 dark:text-surface-200 leading-normal mb-8 text-center lg:text-center">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+</script>
